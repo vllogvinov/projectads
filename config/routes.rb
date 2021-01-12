78007789f23d85b9870f85e1c685c73c5d546f7e
Ваше Имя <you@example.com>
2021-01-13 02:36:30 +0300

    Devise is translated

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :announcements
   devise_for :users
-  # root 'sessions#new', scope: :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'announcements#index'
   resources :users do
     member do
