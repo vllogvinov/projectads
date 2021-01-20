a44752a73999caa5e8a5abe0c170a7ceb951f182
Ваше Имя <you@example.com>
2021-01-20 14:14:22 +0300

    Added bootstrap hat, card-viewed index page, activeadmin bootstrap theme and style of  pagination

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -3,36 +3,28 @@
 class AnnouncementsController < ApplicationController
   before_action :set_announcement, only: %i[show edit update destroy]
 
-  # GET /announcements
-  # GET /announcements.json
   def index
     @announcements = Announcement.published.includes(image_attachment: :blob).page(params[:page]).per(6).desc_order
   end
 
-  # GET /announcements/1
-  # GET /announcements/1.json
   def show
     if @announcement.rejected?
       flash[:notice] = t('flashes.reject_notice')
     end
   end
 
-  # GET /announcements/new
   def new
     @announcement = Announcement.new(user_id: current_user.id)
   end
 
-  # GET /announcements/1/edit
   def edit; end
 
-  # POST /announcements
-  # POST /announcements.json
   def create
     @announcement = current_user.announcements.new(announcement_params)
     @announcement.send_to_moderate if params[:post_button].present?
     respond_to do |format|
       if @announcement.save
-        format.html { redirect_to @announcement, notice: 'Announcement was successfully created.' }
+        format.html { redirect_to @announcement, notice: t('announcements.notices.created') }
         format.json { render :show, status: :created, location: @announcement }
       else
         format.html { render :new }
@@ -41,13 +33,11 @@ class AnnouncementsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /announcements/1
-  # PATCH/PUT /announcements/1.json
   def update
     respond_to do |format|
       if @announcement.update(announcement_params)
         status_definition
-        format.html { redirect_to @announcement, notice: 'Announcement was successfully updated.' }
+        format.html { redirect_to @announcement, notice: t('announcements.notices.updated') }
         format.json { render :show, status: :ok, location: @announcement }
       else
         format.html { render :edit }
@@ -56,24 +46,20 @@ class AnnouncementsController < ApplicationController
     end
   end
 
-  # DELETE /announcements/1
-  # DELETE /announcements/1.json
   def destroy
     @announcement.destroy
     respond_to do |format|
-      format.html { redirect_to announcements_url, notice: 'Announcement was successfully destroyed.' }
+      format.html { redirect_to announcements_url, notice: t('announcements.notices.deleted') }
       format.json { head :no_content }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_announcement
     @announcement = Announcement.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def announcement_params
     params.require(:announcement).permit(:title, :content, :announcement_type, :status, :phone, :image, :post_button)
   end
